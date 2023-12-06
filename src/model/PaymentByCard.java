4659b86ee741ef1597778cb12df55d74969b2589
Gaukhar <gaukhar.kashaganova@khresterion.com>
2023-12-06 23:12:07 +0100

    adds class PaymentByCard

---
+++ src/model/PaymentByCard.java
@@ -0,0 +1,37 @@
+package model;
+
+public class PaymentByCard {
+    String cardNumber;
+    String password;
+    int amount;
+
+    public PaymentByCard(String cardNumber, String password, int amount){
+        this.cardNumber = cardNumber;
+        this.password = password;
+        this.amount = amount;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
